c5c55fd6c51bed41cdd9239883b7838e14d45b3b
Andrea Guerrero <guerreroaanathalia@gmail.com>
2024-04-26 14:18:36 -0500

    navbarlisto

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
 
 const Layout = () => {
   return (
-    <div>
-      Layout
-        <Outlet/>
-    </div>
-  )
-}
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  );
+};
 
-export default Layout
+export default Layout;
