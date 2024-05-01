772844d3c1e5aec97fd90624e588813956cceca7
Valentina Cedano <valentina.cedano57@gmail.com>
2024-04-30 22:53:43 -0500

    editando sombra

--- src/pages/Perfil.jsx
+++ src/pages/Perfil.jsx
@@ -18,7 +18,7 @@ const Perfil = () => {
       <div className='flex flex-row  w-full'>
         {/* Renderizar el componente seleccionado al lado de las opciones */}
         <aside className='flex-col w-1/3.2  w-80 h-screen'>
-          <nav className='flex flex-col gap-5 pt-7 font-body text-m mx-3 h-72 bg-white rounded-lg items-center shadow shadow-gray-500 mt-11 mx-1'>
+          <nav className='flex flex-col gap-5 pt-7 font-body text-m mx-3 h-72 bg-white rounded-lg items-center shadow shadow-gray-300 mt-11 mx-1'>
             
             <li className={`list-none text-blue-700  w-4/5 h-10 px-2 pt-2 rounded-md bg-blue-100 hover:text-blue-500 font-semibold hover:bg-blue-200 `} onClick={() => handleClick(<MisReseñas />)}>Reseñas</li>
             <li className={`list-none text-blue-700 w-4/5 h-10 px-2 pt-2 rounded-md bg-blue-100 hover:text-blue-500 font-semibold hover:bg-blue-200 `}  onClick={() => handleClick(<Planes />)}>Planes</li>
