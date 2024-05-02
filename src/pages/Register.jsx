9176668139ecff784e8abcaf57c01261faa4c0e3
Valentina Cedano <valentina.cedano57@gmail.com>
2024-05-01 20:46:18 -0500

    Pagina registro

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,47 +1,36 @@
 import React from 'react'
-import backgroundImage from "../assets/fondoRegister.png";
+import backgroundImage from "../assets/fondoRegister.jpeg";
 const Register = () => {
   return (
-    <div className="bg-cover bg-center h-screen" style={{ backgroundImage: `url(${backgroundImage})` }}>
-      <nav class="bg-white">
-        <div class="relative flex h-16 items-center justify-between">
-
-          <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
-            <button type="button" class="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-              <span class="absolute -inset-1.5"></span>
-              <span class="sr-only">View notifications</span>
-              <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
-              </svg>
-            </button>
-
-
-          </div>
-        </div>
-
+    <div className="bg-cover bg-center  flex flex-col items-center  h-screen " style={{ backgroundImage: `url(${backgroundImage})` }}>
+      
+      <nav className="bg-white h-16 flex justify-center pt-5 w-screen ">
+          <img  className = "w-36 h-8" src= "src\assets\logo.png"/>
       </nav>
 
 
-      <form class="max-w-sm mx-auto">
-        <div class="mb-5">
-          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
+      <form class="w-2/4 h-5/6 mt-7 pt-5 flex flex-col items-center font-body bg-white rounded-2xl">
+        <h1 className="font-title text-4xl  mb-6" style={{ color: 'rgb(5 150 105)' }}> Registrate </h1>
+        <div class="mb-5 w-2/3"> 
+          <label for="name" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-semibold " style={{ color:'rgb(13 148 136) ' }}>Nombre Completo</label>
+          <input type="nombre" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
+        </div>
+        <div class="mb-5 w-2/3">
+          <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-semibold " style={{ color:'rgb(13 148 136) ' }} >Correo electrónico</label>
           <input type="email" id="email" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@flowbite.com" required />
+          
         </div>
-        <div class="mb-5">
-          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your password</label>
-          <input type="password" id="password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
-        </div>
-        <div class="mb-5">
-          <label for="repeat-password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Repeat password</label>
+        <div class="mb-5 w-2/3">
+          <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white font-semibold " style={{ color:'rgb(13 148 136) ' }}>Contraseña</label>
           <input type="password" id="repeat-password" class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
         </div>
-        <div class="flex items-start mb-5">
+        <div class="flex items-start mb-5 w-2/3">
           <div class="flex items-center h-5">
             <input id="terms" type="checkbox" value="" class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800" required />
           </div>
-          <label for="terms" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">I agree with the <a href="#" class="text-blue-600 hover:underline dark:text-blue-500">terms and conditions</a></label>
+          <label for="terms" class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300">Al registrarme, acepto los Términos de uso y la Política de privacidad </label>
         </div>
-        <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Register new account</button>
+        <button type="submit" class="text-white w-72 bg-green-500 font-body hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Ingresar </button>
       </form>
 
     </div>
