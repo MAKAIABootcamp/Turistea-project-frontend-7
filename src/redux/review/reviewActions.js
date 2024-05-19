b2eeeae2986742ea2505fd73ce471fe0913dc0ce
Sara14 <88954064+Sweet17n@users.noreply.github.com>
2024-05-19 02:14:49 -0500

    Update reviewActions.js
    correcion de palabras en español a ingles

--- src/redux/review/reviewActions.js
+++ src/redux/review/reviewActions.js
@@ -1,26 +1,26 @@
 import { addDoc, collection } from "firebase/firestore";
 import { db } from "../../firebase/firebaseConfig"; // Ajusta la ruta según sea necesario
-import { reseñasRequest, reseñasFail, addReseña } from "./reviewSlice";
+import { reviewRequest, reviewFail, addreview } from "./reviewSlice";
 
 const COLLECTION_NAME = "Reviews"; // Nombre de la colección
 const collectionRef = collection(db, COLLECTION_NAME); // Referencia de la colección
 
-export const actionAddReseña = (newReseña) => {
+export const actionAddReview = (newReview) => {
   return async (dispatch) => {
-    dispatch(reseñasRequest());
+    dispatch(reviewRequest());
     try {
-      console.log("Adding new review to Firestore:", newReseña);
-      const docRef = await addDoc(collectionRef, newReseña);
+      console.log("Adding new review to Firestore:", newReview);
+      const docRef = await addDoc(collectionRef, newReview);
       dispatch(
-        addReseña({
+        addreview({
           id: docRef.id,
-          ...newReseña,
+          ...newReview,
         })
       );
       console.log("Review successfully added with ID:", docRef.id);
     } catch (error) {
       console.error("Error adding review to Firestore:", error);
-      dispatch(reseñasFail(error.message));
+      dispatch(reviewFail(error.message));
     }
   };
 };
