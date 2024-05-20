2e09f67e00a8fdc432a5c5047f0c22042cfd025b
Sara14 <88954064+Sweet17n@users.noreply.github.com>
2024-05-19 20:20:57 -0500

    Create store.js

---
+++ src/redux/review/store.js
@@ -0,0 +1,13 @@
+import { configureStore } from '@reduxjs/toolkit';
+import reviewReducer from './reviewSlice'; // Asegúrate de que esta ruta es correcta
+
+const store = configureStore({
+  reducer: {
+    review: reviewReducer,
+  },
+});
+
+export default store;
+
+
+
