72b0701a8a4c38f4391c827900e0b3fddb1a7d41
Andrea Guerrero <guerreroaanathalia@gmail.com>
2024-05-22 18:27:07 -0500

    configuracion para el despliegue

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import store from "./redux/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/Turistea-project-frontend-7">
         <AppRouter />
       </BrowserRouter>
     </Provider>
