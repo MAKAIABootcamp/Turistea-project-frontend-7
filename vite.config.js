0119ba8d9ab07a6ed12d2d0dd4b35a56100268d8
Andrea Guerrero <guerreroaanathalia@gmail.com>
2024-05-23 13:49:21 -0500

    cambios para funcionalidad

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/Turistea-project-frontend-7/",
+  base:"https://makaiabootcamp.github.io/Turistea-project-frontend-7/",
   plugins: [react()],
 })
