92df1ac26c6152dfc03a74641a5d6cbcfc487d42
Valentina Cedano <valentina.cedano57@gmail.com>
2024-05-23 12:11:54 -0500

    cambios

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ import store from "./redux/store";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter basename="/Turistea-project-frontend-7/">
+      <BrowserRouter>
         <AppRouter />
       </BrowserRouter>
     </Provider>
